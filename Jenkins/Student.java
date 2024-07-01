0784ee4e6fd585420a41cc899f09a1b617933646
Rajat Gupta <96593277+Rajatrpg@users.noreply.github.com>
2024-07-01 22:13:50 +0530

    added student class

---
+++ Jenkins/Student.java
@@ -0,0 +1,14 @@
+package Jenkins;
+
+public class Student {
+
+    public Student(){
+    
+    System.out.println("Creating student object");
+    }
+
+    public void sayHello(){
+        System.out.println("Hi , i am Student");
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+import Jenkins.Student;
+public class Main{
+
+    public static void main(String[] args){
+
+        System.out.println("This is java program!!");
+        System.out.println("output from java program");
+        System.out.println(new Date());
+        System.out.println("Hello");
+        System.out.println("Added one more file");
+        Student student = new Student();
+        student.sayHello();
+}
+}
